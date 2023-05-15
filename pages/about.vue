829834e277cc76918cad62553c1a0736c6d1dced
besscroft <besscroft@foxmail.com>
2023-05-15 17:29:38 +0800

    style:样式调整

--- pages/about.vue
+++ pages/about.vue
@@ -5,18 +5,11 @@ definePageMeta({
 </script>
 
 <template>
-    <div pt-4 mxa>
-        <p class="text-2xl text-center font-semibold">鹤鸣</p>
-
-        <p class="text-left mx-2 mt-4">
-            鹤鸣于九皋，声闻于野。鱼潜在渊，或在于渚。乐彼之园，爰有树檀，其下维萚。他山之石，可以为错。
-        </p>
-
-        <p class="text-left mx-2 mt-2">
-            鹤鸣于九皋，声闻于天。鱼在于渚，或潜在渊。乐彼之园，爰有树檀，其下维谷。他山之石，可以攻玉。
-        </p>
-
-        <p class="text-right mt-2">——《诗经‧小雅-鸿雁之什‧鹤鸣》</p>
+    <h3 class="font-serif text-3xl border-4 light:border-pink-100 w-36 mxa">
+        商务合作
+    </h3>
+    <div class="mxa mt-4">
+        联系邮箱（其他）：besscroft@foxmail.com
     </div>
 </template>
 
