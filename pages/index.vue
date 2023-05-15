ff8392412955186aaf7c5f6ea600d6a64fcdf200
besscroft <besscroft@foxmail.com>
2023-05-15 17:34:52 +0800

    style:样式调整

--- pages/index.vue
+++ pages/index.vue
@@ -14,7 +14,7 @@ definePageMeta({
             </h1>
         </NuxtLink>
 
-        <div class="py-32 w-180 mxa">
+        <div class="py-24 max-w-xl mxa">
             <p class="text-2xl text-center font-semibold">鹤鸣</p>
 
             <p class="text-left mx-2 mt-4">
