4594c54570bb1cf297f33f6767a83ecaf5d35619
besscroft <besscroft@foxmail.com>
2023-11-04 22:48:08 +0800

    style:样式调整

--- pages/gallery.vue
+++ pages/gallery.vue
@@ -1,34 +1,76 @@
 <script setup lang="ts">
-const info = ref([
+const openInfo = ref([
   {
     title: 'DiyFile',
     desc: '一款好看的在线文件列表程序，由 Spring Boot 3 和 Vue 驱动。',
-    href: 'https://github.com/besscroft/diyfile',
-    target_title: 'GitHub',
+    href: 'https://demo.besscroft.com/',
+    github: 'https://github.com/besscroft/diyfile',
   },
   {
     title: 'HeMing Fast',
     desc: '基于 SpringBoot3 + Nuxt 3 + Vuetify 3 的脚手架！',
-    href: 'https://github.com/besscroft/heming-fast',
-    target_title: 'GitHub',
+    href: 'https://fast.heming.dev/',
+    github: 'https://github.com/besscroft/heming-fast',
   },
   {
     title: '嗯学英语',
     desc: '嗯学英语，学英语，就嗯学！',
-    href: 'https://github.com/besscroft/enstudy',
-    target_title: 'GitHub',
+    href: 'https://enstudy.heming.dev/',
+    github: 'https://github.com/besscroft/enstudy',
   },
+  {
+    title: 'Pisces-Cloud',
+    desc: '一站式微服务解决方案，基于 openJDK 17、Spring Cloud 2021.0.1 & Alibaba 2021.0.1.0 + Spring Security OAuth2 + PostgreSQL + Mybatis-Plus 构建。',
+    href: 'https://pisces.besscroft.com/',
+    github: 'https://github.com/besscroft/pisces-cloud',
+  },
+  {
+    title: 'nuxt-h5',
+    desc: 'Nuxt 3 H5 Starter',
+    href: 'https://nuxt-h5.vercel.app/',
+    github: 'https://github.com/besscroft/nuxt-h5',
+  },
+])
+
+const noOpenInfo = ref([
   {
     title: 'duduke',
     desc: '蹦蹦火花，是可莉的核武器啦！',
     href: '#',
-    target_title: 'N/A',
+    github: 'N/A',
   },
   {
     title: 'duduke-clip',
     desc: '嘟嘟可 CLIP 模型炼丹系统！',
     href: '#',
-    target_title: 'N/A',
+    github: 'N/A',
+  },
+  {
+    title: '云淑故事集',
+    desc: '收录我拍的各种照片...',
+    href: 'https://kamera.heming.dev/',
+    github: 'N/A',
+  },
+])
+
+const openArchiveInfo = ref([
+  {
+    title: 'heming-rss',
+    desc: '一款 Material Design 风格的 RSS 阅读器，支持 ChatGPT 和自定义订阅。',
+    href: 'https://heming-rss.vercel.app/',
+    github: 'https://github.com/besscroft/heming-rss',
+  },
+  {
+    title: 'duduke-clip',
+    desc: '嘟嘟可 CLIP 模型炼丹系统！',
+    href: '#',
+    github: 'N/A',
+  },
+  {
+    title: '云淑故事集',
+    desc: '收录我拍的各种照片...',
+    href: 'https://kamera.heming.dev/',
+    github: 'N/A',
   },
 ])
 
@@ -38,13 +80,46 @@ definePageMeta({
 </script>
 
 <template>
-  <div grid grid-cols-1 gap-3 md:grid-cols-3 md:gap-8 mxa md:mt-8 md:px-20>
-    <div :key="item" v-for="item in info" flex flex-col items-center h-48 rounded-md border-green-400 border-4 px-4>
-      <div h-12 flex items-center><p text-xl>{{ item.title }}</p></div>
-      <div h-24 p-2><p>{{ item.desc }}</p></div>
-      <a h-12 p-2 icon-btn rel="noreferrer" :href="item.href" target="_blank" :title="item.target_title">
-        <div animate-bounce scale-150 i-carbon-send-alt />
-      </a>
+  <div>
+    <div w-full text-center h-12 text-xl>开源</div>
+    <div md:px-4 lg:px-8 xl:px-12 grid md:gap-2 lg:gap-4 xl:gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4>
+      <div
+        :key="item.title"
+        v-for="item in openInfo"
+        flex flex-col items-center h-48 rounded-md border-green-400 border-4 px-4 my-1
+        hover:-translate-y-1 hover:scale-105 hover:transition duration-300
+      >
+        <div h-12 flex items-center><p text-xl>{{ item.title }}</p></div>
+        <div h-24 p-1><p>{{ item.desc }}</p></div>
+        <div h-12 p-2 space-x-2>
+          <a icon-btn v-if="item.href !== '#'" rel="noreferrer" :href="item.href" target="_blank" title="Website">
+            <div animate-bounce scale-150 i-carbon-send-alt />
+          </a>
+          <a icon-btn v-if="item.github !== 'N/A'" rel="noreferrer" :href="item.github" target="_blank" title="GitHub">
+            <div animate-bounce scale-150 i-carbon-logo-github />
+          </a>
+        </div>
+      </div>
+    </div>
+    <div w-full text-center h-12 text-xl>闭源</div>
+    <div md:px-4 lg:px-8 xl:px-12 grid md:gap-2 lg:gap-4 xl:gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4>
+      <div
+        :key="item.title"
+        v-for="item in noOpenInfo"
+        flex flex-col items-center h-48 rounded-md border-green-400 border-4 px-4 my-1
+        hover:-translate-y-1 hover:scale-105 hover:transition duration-300
+      >
+        <div h-12 flex items-center><p text-xl>{{ item.title }}</p></div>
+        <div h-24 p-1><p>{{ item.desc }}</p></div>
+        <div h-12 p-2 space-x-2>
+          <a icon-btn v-if="item.href !== '#'" rel="noreferrer" :href="item.href" target="_blank" title="Website">
+            <div animate-bounce scale-150 i-carbon-send-alt />
+          </a>
+          <a icon-btn v-if="item.github !== 'N/A'" rel="noreferrer" :href="item.github" target="_blank" title="GitHub">
+            <div animate-bounce scale-150 i-carbon-logo-github />
+          </a>
+        </div>
+      </div>
     </div>
   </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -9,17 +9,15 @@ definePageMeta({
     <div max-w-xl mxa>
       <p text-2xl text-center font-semibold>鹤鸣</p>
 
-      <p text-left mx-2 mt-4>
+      <p text-left mx-2 mt-4 indent-8 leading-loose>
         鹤鸣于九皋，声闻于野。鱼潜在渊，或在于渚。乐彼之园，爰有树檀，其下维萚。他山之石，可以为错。
       </p>
 
-      <p text-left mx-2 mt-2>
+      <p text-left mx-2 mt-2 indent-8 leading-loose>
         鹤鸣于九皋，声闻于天。鱼在于渚，或潜在渊。乐彼之园，爰有树檀，其下维谷。他山之石，可以攻玉。
       </p>
 
       <p text-right mt-2>——《诗经‧小雅-鸿雁之什‧鹤鸣》</p>
     </div>
-
-    <p max-w-xl mxa>装修中...</p>
   </div>
 </template>
